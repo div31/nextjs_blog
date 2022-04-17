5a76c4935f789a5352bf6cb22cb88ab4294cbb1d
divya <divyasuri343@gmail.com>
2022-04-17 21:37:50 +0530

    added new comps

--- services/index.js
+++ services/index.js
@@ -3,7 +3,8 @@ import { request, gql } from 'graphql-request';
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 console.log(graphqlAPI)
 
-export const getPosts = async () => { debugger
+export const getPosts = async () => {
+    debugger
     const query = gql`
         query MyQuery {
         postsConnection {
@@ -38,4 +39,62 @@ export const getPosts = async () => { debugger
     const result = await request(graphqlAPI, query);
     console.log(result)
     return result.postsConnection.edges;
+}
+
+export const getRecentPosts = async () => {
+    const query = gql`
+        query GetPostDetails() {
+            posts(
+                orderBy: createdAt_ASC
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query);
+    console.log(result)
+    return result.posts;
+}
+
+export const getSimilarPosts = async () => {
+    const query = gql`
+        query GetPostDetails($slug: String!, $categories: [String!]) {
+            posts(
+                where: { slug_not: $slug, AND: {categories_some: { slug_in: $categories }}}
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query);
+    console.log(result)
+    return result.posts;
+}
+
+export const getCategories = async () => {
+    const query = gql`
+        query GetCategories {
+            categories {
+                name
+                slug
+            }
+        }
+    `
+        const result = await request(graphqlAPI, query);
+        console.log(result)
+        return result.categories;
 }
